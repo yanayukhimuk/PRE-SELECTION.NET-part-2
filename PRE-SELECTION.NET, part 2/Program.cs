b2c906582b45b86b622e673ab1be2c9abb53fd6b
Yana Yukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2021-09-27 21:01:48 +0300

    Add project files.

---
+++ PRE-SELECTION.NET, part 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PRE_SELECTION.NET__part_2
+{
+    class Program
+    {
+        interface YandexTaxi
+        {
+            void SetCarModel(string name);
+            void SetPricePerKM(double price);
+
+            void SetDistance(int km);
+            void SetTripTime(int min);
+
+            void SetArrivalTime(int h, int m, int s);
+            void ShowCar();
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
